01f96bc6248cf6f715a33808fb0ee9315b35a691
Javier Martínez <contacto@javiermrt.com>
2022-06-12 19:13:28 +0200

    Primer commit

---
+++ src/modules/daybook/components/Entry.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="entry-container mb-3 pointer p-2"
+    @click="$router.push({ name: 'entry', params: { id: 10} })"
+  >
+    <!-- Título -->
+    <div class="entry-title d-flex">
+        <span class="text-success fs-5 fw-bold">12</span>
+        <span class="mx-1 fs-5">Junio</span>
+        <span class="mx-2 fw-light">2022, sábado</span>
+    </div>
+    <div class="entry-description">
+        Mollit duis tempor id aliqua ea culpa est aliqua excepteur. Nisi anim ad duis labore laborum reprehenderit commodo consectetur. Quis Lorem veniam veniam amet anim id dolore ut eiusmod ex. Eu nisi dolore irure culpa ipsum magna eu. Aute commodo do officia sint incididunt id. Occaecat ullamco nisi culpa culpa ex qui ullamco nulla quis eiusmod pariatur dolore. Laboris exercitation do officia consectetur aliquip magna aliquip.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.entry-container {
+    border-bottom: 1px solid #2c3e50;
+    transition: 0.2s all ease-in;
+
+    &:hover {
+        background-color: lighten($color: grey, $amount:  45);
+        transition: 0.2s all ease-in;
+    }
+
+    .entry.description {
+        font-size: 12px;
+    }
+}
+</style>
---
+++ src/modules/daybook/store/journal/state.js
@@ -0,0 +1,24 @@
+
+export default () => ({
+    isLoading: true,
+    entries: [
+        {
+            id: new Date().getTime(),
+            date: new Date().toDateString(),
+            text: 'Esse anim labore et elit. Mollit est ea eiusmod dolor ea nulla sunt magna do minim officia. Eiusmod aliqua eiusmod fugiat tempor. Anim eiusmod tempor ea id exercitation magna nisi exercitation ullamco velit cupidatat dolor id.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 1000,
+            date: new Date().toDateString(),
+            text: 'Ex anim consectetur do excepteur aliquip ad. Laborum id laborum cillum amet in voluptate quis mollit veniam anim laborum proident amet do. Eiusmod sint id Lorem ea reprehenderit.',
+            picture: null,
+        },
+        {
+            id: new Date().getTime() + 2000,
+            date: new Date().toDateString(),
+            text: 'Quis do cillum adipisicing magna aliqua reprehenderit enim commodo incididunt proident. Aliqua ullamco nostrud in ipsum fugiat nulla do aliqua ullamco. Esse nulla incididunt officia laborum consequat dolor culpa adipisicing.',
+            picture: null,
+        },
+    ]
+})
